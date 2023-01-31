674c973d0dbe2c3931397912e4c01f080d1d4bed
viktor ojemeni <vykeoj@gmail.com>
2023-01-31 05:52:43 +0100

    logging events

--- index.js
+++
@@ -1,6 +0,0 @@
-const { format } = require("date-fns");
-const { v4: uuid } = require("uuid");
-
-console.log(uuid())
-// console.log(format(new Date(), 'yyyyMMdd'));
-console.log("This is testing")
