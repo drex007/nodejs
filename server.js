d89581e6660d25c28efa8e8f91589571fb95d2b5
drex007 <vykeoj@gmail.com>
2023-02-06 13:03:56 +0100

    node webserver

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log("Hi this is my server ");
+// const logEvent = require('./logEvent');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
+const EventEmitter = require('events');
+
+class Emitter extends EventEmitter { }
+
+//Initialize eventEmitter comment
+const myEmitter = new Emitter();
+// myEmitter.on('log', (msg) => logEvent(msg));
+// myEmitter.emit('log', 'Log event is emitted from index');
+
+
+const PORT = process.env.PORT || 3500;
+
+const server = http.createServer((req, res)=>{
+    console.log(req.url, req.method)
+});
+
+server.listen(PORT, ()=> console.log(`server is runnung on port ${PORT}`))
