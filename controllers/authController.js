bc05ca85c7acc131042f892be1b9f1ff04b84c25
drex007 <vykeoj@gmail.com>
2023-02-21 10:00:44 +0000

    authentication in odejs

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,6 +6,10 @@ const usersDB = {
 }
 
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const fsPromises = require('fs').promises;
+const path = require('path');
 
 const handleLogin = (req, res) => {
     const { user, pwd } = req.body; // Get params from body
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const authController  = require('../controllers/authController');
-
+//require('crypto').randomBytes(64).toString('hex')
 router.post('/', authController.handleLogin);
 
 module.exports = router;
